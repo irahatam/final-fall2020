2e289645060593797876b64dff7e80fa75571e4c
irahatam <matahari@nyu.edu>
2020-12-05 20:47:34 -0500

    added frontend and backend skeleton

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
 // Backend App for Final Project
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+const indexRoute = require("./routes/index.js");
+
+app.use("/", indexRoute);
+
+app.listen(port, () => console.log(`Backend is running at port:${port}`));
