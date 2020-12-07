97acc53a61081f0b4e62801128fe7a232e6c4ad7
irahatam <matahari@nyu.edu>
2020-12-06 21:12:46 -0500

    added sample api

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,18 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const indexRoute = require("./routes/index.js");
+// Add firebase below
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// Add more routes below
 app.use("/", indexRoute);
 
 app.listen(port, () => console.log(`Backend is running at port:${port}`));
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Testing backend"));
+const sampleJSON = [
+  {
+    name: "Matahari",
+    role: "Student",
+    pet: "Nala",
+  },
+  {
+    name: "James",
+    role: "Teacher",
+    pet: "Artemis",
+  },
+];
+
+router.get("/", (req, res) => res.send(sampleJSON));
 
 module.exports = router;
